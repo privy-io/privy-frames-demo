d978b3d147242589bb1bc2f4dbb5fc4d8d467b45
Ankush Swarnakar <ankushswar1@gmail.com>
2024-01-31 21:12:04 -0500

    Typo

--- app/lib/nft.ts
+++ app/lib/nft.ts
@@ -3,7 +3,7 @@ import { mnemonicToAccount } from "viem/accounts";
 import { optimismSepolia } from "viem/chains";
 
 const NFT_WALLET_MNEMONIC = process.env.NFT_WALLET_MNEMONIC as string;
-const NFT_CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_NFT_CONTRACT_ADDRESS as `0x${string}`; // Optimism Sepolia Testnet
+const NFT_CONTRACT_ADDRESS = process.env.NFT_CONTRACT_ADDRESS as `0x${string}`; // Optimism Sepolia Testnet
 
 const MINT_ABI = {
     "inputs": [
